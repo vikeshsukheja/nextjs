eb8d386ddffa1dda0e03f3ed631fea7bfda5fa01
vikeshsukheja <vikesh666@gmail.com>
2024-05-31 02:37:52 +0530

    Update page.tsx

--- app/Q3/page.tsx
+++ app/Q3/page.tsx
@@ -2,25 +2,25 @@ export default function CustomerResponse() {
   return (
     <div>
       <p>3. A customer has reached out asking, “When should I choose to use Edge Functions, Serverless Functions, or Edge Middleware with Vercel?” Write a reply to the customer.</p>
-
+<br></br>
       <p>Ans) Hi Mr. Customer,</p>
-
+<br></br>
       <p>Thanks for reaching out. Please find my response below:</p>
-
+<br></br>
       <p>Serverless Functions, Edge Functions, and Edge Middleware are distinct methods for executing code on Vercel, each serving different purposes and possessing unique characteristics.</p>
-
+<br></br>
       <p>Serverless Functions are conventional functions powered by Lambdas. They operate within a single region and are well-suited for computationally intensive tasks. These functions offer complete compatibility with Node.js, granting access to all Node.js APIs. However, they may have a longer boot time compared to functions utilizing the Edge runtime.</p>
-
+<br></br>
       <p>Edge Functions resemble Serverless Functions, but they are replicated across the Edge Network. Consequently, whenever the function is invoked, the region closest to the request will execute the function, resulting in significantly reduced latency. Edge Functions are executed after the cache and are particularly useful for specific, dynamic components of your website once the page has loaded. However, they employ a streamlined runtime that does not support all Node.js APIs.</p>
-
+<br></br>
       <p>Edge Middleware, on the other hand, is a type of function situated on the Edge Network, preceding your cached content. When a user sends a request to your site, it first encounters the middleware, which examines the request and subsequently sends an appropriate response from the server. Middleware proves valuable in implementing functionality that may be shared by multiple URL paths within a Project, such as authentication, before forwarding the request to the underlying resource (such as a page or asset) at that particular path.</p>
-
+<br></br>
       <p>To summarize, Serverless Functions excel in handling intensive computational tasks, Edge Functions excel in delivering dynamic content swiftly, and Edge Middleware excels in managing requests prior to reaching cached content. The selection among these three options is contingent upon your specific requirements and the characteristics of your application.</p>
-
+<br></br>
       <p>Hope this helps you clear your doubt. Feel free to reach for any other queries that you might have. I will also be happy to schedule a meeting with our technical experts to go through further details.</p>
-
+<br></br>
       <p>Have a great day!</p>
-
+<br></br>
       <p>Best Regards,<br/>Vikesh Sukheja<br/>Customer Success Manager, Vercel</p>
     </div>
   );
