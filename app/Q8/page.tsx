89336a639622317c67d15dd80e7e2f0ec7999ae3
vikeshsukheja <vikesh666@gmail.com>
2024-05-31 04:16:31 +0530

    Create page.tsx

---
+++ app/Q8/page.tsx
@@ -0,0 +1,14 @@
+export default function A8() {
+  return (
+    <div>
+      <p>8. How could we improve or alter this familiarization exercise?</p>
+      <p>Thanks for sharing this assignment with me. I thought it was a great experience to help myself familiarize with your product. I liked how one gets to interact with the product and gets hands-on experience with Vercel and its offerings.</p>
+      <p>Few recommendations that I can suggest for improving or altering the experience:</p>
+      <ul>
+        <li>For people coming in with different backgrounds/non-tech background, they might find it slightly difficult to create or deploy pages on Vercel and might require step-by-step documentation to use Vercel/Github</li>
+        <li>There could be more situation-based questions (if that is the intention) to get to know how an individual reacts to it for a given role.</li>
+      </ul>
+      <p>Thanks for allowing me to go through this experience. I would be glad to get your feedback on my responses and how could I further improve upon the way I approached this exercise.</p>
+    </div>
+  );
+}
