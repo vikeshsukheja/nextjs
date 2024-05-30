658c5a8c70863f48c116da9b0003a4d82beb43b8
vikeshsukheja <vikesh666@gmail.com>
2024-05-30 23:05:53 +0530

    Update Q1.tsx

--- app/Q1.tsx
+++ app/Q1.tsx
@@ -1,19 +1,5 @@
 export default function A1(
-   return (
-   <!DOCTYPE html>
-<html>
-   <head>
-      <title>Q1</title>
-   </head>
-   <body>
-      <h1> 1.	Describe how you solved a challenge that one of your previous teams faced.
-How did you determine your solution was successful?
-</h1>
-      <p> Ans) In my previous team as a Project Manager, there were lot of issues which my teams faced. One such issue that I can recollect is: There was a requirement from a customer to work on network architecture design for one of their sites. We had assigned a dedicated team to work on this requirement. But we got regular feedback from engineering team that, we are unable to focus and make progress, due to change in customer priorities on a regular basis, and this was leading to cost consumption and deviation from project timelines. 
-
-To resolve this issue, we decided there was active communication needed with customer and a workshop was required to initially identify the real requirement from customer at a high level. Though it was agreed, some specific requirement will keep on modifying. Hence, we agreed to go ahead with the agile methodology instead of waterfall approach. We deployed scrum methodology, and got a stakeholder from customer team, to ensure we have someone who can act as a product owner. We received quicker feedback from customer in terms of work done and adopted to the changes they proposed. I acted as a scrum master to ensure the progress was constantly showcased to the customer, product owner worked closely with team, and we achieved desired outcomes in sprints planned. At the end, the shift in approach to agile methodology and the initial workshop to identify customer requirements really paved the way for successful project. 
-
-      </p>
-   </body>
-</html> );
+return (
+   <html><p>H1</p></html>
+);
 }
