358c6fa37e23b7be7b45eb6efe2c847ad7872fdc
vikeshsukheja <vikesh666@gmail.com>
2024-05-31 03:58:54 +0530

    Update page.tsx

--- app/Q6/page.tsx
+++ app/Q6/page.tsx
@@ -6,12 +6,12 @@ recommended options for third-party integrations for Monitoring or Logging.
 Knowing that:
 a. Vercel has in-house observability products,
 b. Vercel likes to guide on processes, but
-c. Vercel doesn’t like to steer customers towards one solution or another, but
+c. Vercel doesn&rsquo;t like to steer customers towards one solution or another, but
 demonstrates what the product can do.
 How would you respond to the customer?
     </p>
     <br></br>
-   <p>Hi Mr. Customer,</p>
+     <p>Hi Mr. Customer,</p>
     <p>Thanks for reaching out. I hope you have had a good experience so far with us.</p>
     <p>I would gladly suggest you a few 3rd party integration options as requested. However, I would like to highlight that at Vercel we have our own in-house Frontend Observability, which has been trusted by leading companies like The Washington Post, Stripe, Neo, Runway, eBay, Upstart, Notion, and many others. This has been carefully prepared to meet the real needs of the customers and provide them with the best features.</p>
     <p>Observability on Vercel provides you with:</p>
@@ -22,14 +22,14 @@ How would you respond to the customer?
     </ul>
     <p>Here are the following capabilities that you will get with Vercel Frontend Observability depending on your requirements:</p>
     <ul>
-      <li>Runtime Logs: Runtime logs allow you to search, inspect, and share your team's runtime logs at a project level. You can search runtime logs from the deployments section inside the Vercel dashboard. Your log data is retained for 3 days. For longer log storage, you can use log drains.</li>
-      <li>Web Analytics: Web Analytics empowers you with in-depth insights into your website's visitors, showcasing metrics such as top pages, referrers, and demographic data like countries, operating systems, and browser usage.</li>
-      <li>Speed Insights: Vercel Speed Insights provides a convenient way to analyze your site's performance metrics directly within the dashboard, without the need to modify your code. Easily view and filter data based on device type, user percentiles, and reporting windows to better understand and optimize your site's speed and user experience.</li>
+      <li>Runtime Logs: Runtime logs allow you to search, inspect, and share your team&rsquo;s runtime logs at a project level. You can search runtime logs from the deployments section inside the Vercel dashboard. Your log data is retained for 3 days. For longer log storage, you can use log drains.</li>
+      <li>Web Analytics: Web Analytics empowers you with in-depth insights into your website&rsquo;s visitors, showcasing metrics such as top pages, referrers, and demographic data like countries, operating systems, and browser usage.</li>
+      <li>Speed Insights: Vercel Speed Insights provides a convenient way to analyze your site&rsquo;s performance metrics directly within the dashboard, without the need to modify your code. Easily view and filter data based on device type, user percentiles, and reporting windows to better understand and optimize your site&rsquo;s speed and user experience.</li>
       <li>Monitoring: Monitoring allows you to visualize, explore, and track usage and traffic. Using the query editor, you can create custom queries to gain greater insights into your application. This allows you to debug issues and optimize all the projects on your Vercel Team.</li>
       <li>Activity Logs: Activity Logs provide chronologically organized events on your personal or team account. You get an overview of changes to your environment variables, deployments, and more.</li>
       <li>Audit Logs: Audit Logs allow owners to track events performed by other team members. The feature helps you verify who accessed what, for what reason, and at what time. You can export up to 90 days of audit logs to a CSV file.</li>
       <li>Log Drains: Log Drains allow you to export your log data, making it easier to debug and analyze. You can configure Log Drains through the Vercel dashboard or through one of our Log Drains integrations.</li>
-      <li>Open Telemetry Collector: Vercel's OpenTelemetry (OTEL) collector enables seamless transmission of OTEL traces from your Serverless Functions to APM vendors like New Relic. Traces provide valuable insights into your application's performance, helping pinpoint and resolve issues.</li>
+      <li>Open Telemetry Collector: Vercel&rsquo;s OpenTelemetry (OTEL) collector enables seamless transmission of OTEL traces from your Serverless Functions to APM vendors like New Relic. Traces provide valuable insights into your application&rsquo;s performance, helping pinpoint and resolve issues.</li>
     </ul>
     <p>You can refer to <a href="https://vercel.com/docs/observability">https://vercel.com/docs/observability</a> for more information.</p>
     <p>And as promised, here are a few 3rd Party Integrations products that can help you with monitoring and logging:</p>
