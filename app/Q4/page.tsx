dd97e78bedc87856c97d8cbc0926ac528dec4f1f
vikeshsukheja <vikesh666@gmail.com>
2024-05-31 02:42:01 +0530

    Create page.tsx

---
+++ app/Q4/page.tsx
@@ -0,0 +1,49 @@
+export default function EmailResponse() {
+  return (
+    <div>
+     <p>4. A customer has a project on Vercel and they want to redirect the /blog path to
+another website. Write a reply to the customer.
+</p>
+      <br></br>
+      <p>Hi Mr. Customer,</p>
+ <br></br>
+      <p>Trust you are well.</p>
+ <br></br>
+      <p>Please find my response to your query below:</p>
+
+      <p>You have the ability to redirect the /blog path of your main application to a different website by utilizing the <code>rewrites()</code> function in your <code>next.config.js</code> file. Here’s an example how to do this:</p>
+
+      <pre><code>
+      module.exports = {
+        async rewrites() {
+          return [
+            {
+              source: '/blog',
+              destination: 'https://example.com/blog',
+            },
+            {
+              source: '/blog/:vercel',
+              destination: 'https://example.com/blog/:vercel', // Matched parameters can be used in the destination
+            },
+          ]
+        },
+      }
+      </code></pre>
+ <br></br>
+      <p>In this example, when a request is made to <code>/blog</code> on your main app, it will be redirected to the specified URL of the external website. Likewise, a request to <code>/blog/:vercel</code> (where <code>:vercel</code> represents any value) will also be redirected accordingly to the external website's URL.</p>
+ <br></br>
+      <p>You can find more about rewrites on this link: <a href="https://vercel.com/docs/edge-network/rewrites">https://vercel.com/docs/edge-network/rewrites</a></p>
+ <br></br>
+      <p>Additional Sources that might be helpful:</p>
+      <ul>
+        <li><a href="https://vercel.com/docs/edge-network/redirects">https://vercel.com/docs/edge-network/redirects</a></li>
+      </ul>
+ <br></br>
+      <p>Kindly let me know if this was helpful. If not, I will be happy to setup a call with my team, who can further help you with this query.</p>
+ <br></br>
+      <p>Best Regards,<br/>
+      Vikesh,<br/>
+      Customer Success Manager, Vercel</p>
+    </div>
+  );
+}
